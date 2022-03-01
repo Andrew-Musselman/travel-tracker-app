1de7ed2e64fe56d7aa76e74123d1dc2cb55d69a8
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-01 15:35:50 -0700

    Add beforeEach block for user tests

--- test/User-test.js
+++ test/User-test.js
@@ -6,4 +6,27 @@ describe('User', () => {
   let user1;
   let user2;
   let user3;
+  let travelers;
+  beforeEach(() => {
+    let user1 = new User(1)
+    let user2 = new User(2)
+    let user3 = new User(3)
+    let travelers = [
+      {
+   "id": 1,
+   "name": "Ham Leadbeater",
+   "travelerType": "relaxer",
+    }, {
+   "id": 2,
+   "name": "Rachael Vaughten",
+   "travelerType": "thrill-seeker",
+    }, {
+   "id": 3,
+   "name": "Sibby Dawidowitsch",
+   "travelerType": "shopper",
+    },
+    ]
+  });
+
+  
 })

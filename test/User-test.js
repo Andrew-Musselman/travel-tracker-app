ea8279baef73789b9739674addce9f7ae71d6470
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-01 16:28:00 -0700

    Add it blocks for user tests

--- test/User-test.js
+++ test/User-test.js
@@ -28,5 +28,27 @@ describe('User', () => {
     ]
   });
 
-  
+  it('Should be a function', () => {
+    expect(User).to.be.a('function')
+  })
+  it('Should be able to be instantiated', () => {
+    expect(user1).to.be.an.instanceof(User)
+    expect(user2).to.be.an.instanceof(User)
+    expect(user3).to.be.an.instanceof(User)
+  })
+  it('Should have a unique user id', () => {
+    expect(user1.id).to.equal(1)
+    expect(user2.id).to.equal(2)
+    expect(user3.id).to.equal(3)
+  });
+  it('Should have a name', () => {
+    expect(user1.name).to.equal("Ham Leadbeater")
+    expect(user2.name).to.equal("Rachael Vaughten")
+    expect(user3.name).to.equal("Sibby Dawidowitsch")
+  })
+  it('Should have a traveler type property', () => {
+    expect(user1.travelerType).to.equal("relaxer")
+    expect(user2.travelerType).to.equal("thrill-seeker")
+    expect(user3.travelerType).to.equal("shopper")
+  })
 })

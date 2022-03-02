ea640b78d3139f7d8f583eed71d907a6e5ee3b7f
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-02 16:47:35 -0700

    Create beforeEach block for trip tests

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -7,5 +7,38 @@ describe('Trip', () => {
   let trip2;
   let trip3;
   let trips;
-  
+  beforeEach(() => {
+    trips = [
+      {
+    "id": 1,
+    "userID": 44,
+    "destinationID": 49,
+    "travelers": 1,
+    "date": "2022/09/16",
+    "duration": 8,
+    "status": "approved",
+    "suggestedActivities": []
+    }, {
+    "id": 2,
+    "userID": 35,
+    "destinationID": 25,
+    "travelers": 5,
+    "date": "2022/10/04",
+    "duration": 18,
+    "status": "approved",
+    "suggestedActivities": []
+    }, {
+    "id": 3,
+    "userID": 3,
+    "destinationID": 22,
+    "travelers": 4,
+    "date": "2022/05/22",
+    "duration": 17,
+    "status": "approved",
+    "suggestedActivities": []
+    } ];
+    trip1 = new Trip(trips[0]);
+    trip2 = new Trip(trips[1]);
+    trip3 = new Trip(trips[2])
+  })
 })

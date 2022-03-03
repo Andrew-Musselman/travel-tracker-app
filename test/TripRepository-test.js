e2a2d5a8ea5515a2203ef90db1cbf6e9df93fbf4
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-02 18:08:52 -0700

    Create describe block in trip repository test

---
+++ test/TripRepository-test.js
@@ -0,0 +1,9 @@
+import TripRepository from '../src/TripRepository';
+import Trip from '../src/Trip';
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('Trip Repository'. () => {
+  let trips;
+  let tripRepo;
+})

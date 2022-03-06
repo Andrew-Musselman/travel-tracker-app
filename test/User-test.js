a128b1d5213ae2880249a4f2ae3625fd71cb9a35
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-05 17:24:57 -0700

    Wrtie tests for new User methods

--- test/User-test.js
+++ test/User-test.js
@@ -16,6 +16,8 @@ describe('User', () => {
   let user2Trips;
   let user3Trips;
   let user1PastTrips;
+  let destinations;
+  let destinationRepo;
   beforeEach(() => {
     travelerObjs = [
       {
@@ -34,7 +36,7 @@ describe('User', () => {
   trips = [{
      "id": 1,
      "userID": 2,
-     "destinationID": 49,
+     "destinationID": 1,
      "travelers": 1,
      "date": "2022/09/16",
      "duration": 8,
@@ -43,7 +45,7 @@ describe('User', () => {
      }, {
      "id": 2,
      "userID": 2,
-     "destinationID": 25,
+     "destinationID": 2,
      "travelers": 5,
      "date": "2022/10/04",
      "duration": 18,
@@ -52,7 +54,7 @@ describe('User', () => {
      }, {
      "id": 3,
      "userID": 3,
-     "destinationID": 22,
+     "destinationID": 3,
      "travelers": 4,
      "date": "2022/03/04",
      "duration": 17,
@@ -104,13 +106,36 @@ describe('User', () => {
       "status": "pending",
       "suggestedActivities": []
     }];
+    destinations = [{
+      "id": 1,
+      "destination": "Lima, Peru",
+      "estimatedLodgingCostPerDay": 70,
+      "estimatedFlightCostPerPerson": 400,
+      "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+      "alt": "overview of city buildings with a clear sky"
+      }, {
+      "id": 2,
+      "destination": "Stockholm, Sweden",
+      "estimatedLodgingCostPerDay": 100,
+      "estimatedFlightCostPerPerson": 780,
+      "image": "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      "alt": "city with boats on the water during the day time"
+      }, {
+      "id": 3,
+      "destination": "Sydney, Austrailia",
+      "estimatedLodgingCostPerDay": 130,
+      "estimatedFlightCostPerPerson": 950,
+      "image": "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      "alt": "opera house and city buildings on the water with boats"
+      }]
     user1 = new User(travelerObjs[0]);
     user2 = new User(travelerObjs[1]);
     user3 = new User(travelerObjs[2]);
-    tripRepo = new TripRepository(trips)
+    tripRepo = new TripRepository(trips);
+    destinationRepo = new DestinationRepository(destinations);
     user2Trips = [trips[0], trips[1]]
     user3Trips = [trips[2]]
-    user1PastTrips = [trips[3], trips[4]]
+    user1PastTrips = [trips[4]]
   });
 
   it('Should be a function', () => {
@@ -164,12 +189,32 @@ describe('User', () => {
     it('Should be able to tell if the user is currently on a trip', () => {
       user3.getUsersTrips(tripRepo)
       user3.sortTrips()
+      user1.getUsersTrips(tripRepo)
+      user1.sortTrips()
+      expect(user1.currentTrip).to.eql(trips[3])
       expect(user3.currentTrip).to.eql(trips[2])
     })
     it('Should be able to sort pending trips', () => {
       user1.getUsersTrips(tripRepo)
       user1.sortTrips()
       expect(user1.pendingTrips).to.eql([trips[6], trips[7]])
+    })
+    it('Should be able to add destination details for a trip', () => {
+      user2.getUsersTrips(tripRepo)
+      user2.getDestinations(destinationRepo)
+      user3.getUsersTrips(tripRepo)
+      user3.getDestinations(destinationRepo)
+      expect(user2.trips[0].destinationDetails).to.eql(destinations[0])
+      expect(user2.trips[1].destinationDetails).to.eql(destinations[1])
+      expect(user3.trips[0].destinationDetails).to.eql(destinations[2])
+    })
+  })
+  describe('Total Money Spent', () => {
+    it('Should calculate how much a user has spent this year', () => {
+      user2.getUsersTrips(tripRepo)
+      user2.getDestinations(destinationRepo)
+      expect(user2.getTotalSpentThisYear()).to.equal(12056)
+
     })
   })
 })

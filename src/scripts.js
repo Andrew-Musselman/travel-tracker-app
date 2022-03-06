24c71fb44b550bbf17c46e99ecf5773386723e0e
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-06 10:16:58 -0700

    Import fetch call

--- src/scripts.js
+++ src/scripts.js
@@ -3,12 +3,10 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import {fetchData} from './fetchAPI'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import Glide from '@glidejs/glide'
 
 new Glide('.glide').mount()
-
-
-console.log('This is the JavaScript entry file - your code begins here.');

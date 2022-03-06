738de3abef5491ec01f7695397c157193cae4ac4
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-05 17:25:27 -0700

    Add methods to calculate trip costs

--- src/User.js
+++ src/User.js
@@ -20,24 +20,46 @@ class User {
       }
     })
   }
+  addDestinationDetails(destination) {
+    this.trips.forEach(trip => {
+      if (trip.destinationID === destination.id) {
+        trip.destinationDetails = destination
+      }
+    })
+  }
+  getDestinations(destinationRepo) {
+    destinationRepo.data.forEach(destination => {
+      this.addDestinationDetails(destination)
+    })
+  }
   sortTrips() {
     let today = dayjs().format('YYYY/MM/DD')
     this.trips.forEach(trip => {
       if (trip.status === 'approved') {
         let tripStart = dayjs(trip.date);
         let tripEnd = dayjs(trip.date).add(trip.duration, 'day');
-        if (dayjs(today).isAfter(trip.date) && this.pastTrips.indexOf(trip) === -1) {
+        if (dayjs(today).isAfter(tripStart) && dayjs(today).isBefore(tripEnd)) {
+          this.currentTrip = trip
+        } else if (dayjs(today).isAfter(trip.date) && this.pastTrips.indexOf(trip) === -1) {
           this.pastTrips.push(trip)
         } else if (dayjs(today).isBefore(trip.date) && this.futureTrips.indexOf(trip) === -1) {
           this.futureTrips.push(trip)
         }
-        else if (dayjs(today) >= tripStart || dayjs(today) <= tripEnd) {
-          this.currentTrip = trip
-        }
-      } else if (trip.status === 'pending') {
+    } else if (trip.status === 'pending') {
         this.pendingTrips.push(trip)
       }
     })
+  }
+  getTotalSpentThisYear() {
+    let total = this.trips.filter(trip => {
+      if (trip.status === 'approved' && dayjs("2022-01-01").isBefore(trip.date)) {
+        return trip
+      }
+    }).reduce((acc, trip) => {
+      acc += ((trip.destinationDetails.estimatedFlightCostPerPerson * trip.travelers) * 2) + (trip.destinationDetails.estimatedLodgingCostPerDay * trip.duration)
+      return acc
+    }, 0)
+    return Math.round(total * 1.1)
   }
 }
 

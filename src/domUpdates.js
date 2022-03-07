f0581534708e37763379985d5e1b1e717834ddab
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-06 18:21:11 -0700

    Add method to display user info

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,6 +1,7 @@
 let destinationGlideCards = document.querySelector('.glide__slide');
 let tripContainer = document.querySelector('.card-container');
-import Glide from '@glidejs/glide'
+let userWelcome = document.querySelector('#user-welcome');
+let userSpend = document.querySelector('#user-spend');
 
 import User from './User';
 import TripRepository from './TripRepository';
@@ -13,32 +14,23 @@ const domUpdates = {
     tripContainer.innerHTML = ''
     trips.forEach(trip => {
       tripContainer.innerHTML +=
-      `<div class="card">
-        <h3>${trip.destinationDetails.destination}</h3>
+      `<div class="card" tabIndex="0">
+        <h4>${trip.destinationDetails.destination}</h4>
         <img src="${trip.destinationDetails.image}" alt="${trip.destinationDetails.alt}"/>
         <p>Travelers: ${trip.travelers} travelers</p>
         <p>Leaving on: ${trip.date}</p>
         <p>Length: ${trip.duration}</p>
-        <p>Lodging: $${trip.destinationDetails.estimatedLodgingCostPerDay} / night</p>
-        <p>Flight: $${trip.destinationDetails.estimatedFlightCostPerPerson} / person</p>
+        <p>Lodging: $${trip.destinationDetails.estimatedLodgingCostPerDay} per night</p>
+        <p>Flight: $${trip.destinationDetails.estimatedFlightCostPerPerson} per person</p>
         <p>Status: ${trip.status}</p>
       </div>
       `
     })
+  },
+  generateHeader(user) {
+    userWelcome.innerText += ` ${user.name}`;
+    userSpend.innerText = `You've spent $${user.getTotalSpentThisYear()} on trips this year`
   }
 }
-// generateDestinations(destinations) {
-  //   let glide = new Glide('.glide')
-  //   glide.on('mount.before', () => {
-    //     destinations.data.forEach(destination => {
-      //       destinationGlideCards.innerHTML += `
-      //       <li class="glide__slide">
-      //       <p class="destination">${destination.destination}</p>
-      //       <img src=${destination.image} alt=${destination.alt}/>
-      //       </li>`
-      //     })
-      //   }).mount()
-      //   // glide.mount()
-      // }
 
 export default domUpdates;

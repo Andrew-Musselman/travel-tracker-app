b0c4fe806b4d43d9746c9a56829e47750b86d91d
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-07 10:20:37 -0700

    Set up fetch call to post new trip request

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -4,4 +4,18 @@ const fetchData = (path) => {
   .catch(err => console.log(err))
 }
 
+const postData = (path, content) => {
+  return fetch(path, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(content)
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Error requesting trip, please try again')
+    } else {
+      return response.json()
+    }
+  }).catch(err => console.log(err))
+}
+
 export {fetchData}

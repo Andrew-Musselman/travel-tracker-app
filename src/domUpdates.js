b49275a221993577b52b9c0be89bacbfcea9c47e
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-07 17:34:38 -0700

    Add request card to dom

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,12 +2,18 @@ let destinationGlideCards = document.querySelector('.glide__slide');
 let tripContainer = document.querySelector('.card-container');
 let userWelcome = document.querySelector('#user-welcome');
 let userSpend = document.querySelector('#user-spend');
+let tripDestination = document.querySelector('#destination');
+let newTripCard = document.querySelector('#new-trip-card');
+let newTripMessage = document.querySelector('#new-trip-message')
+
 
 import User from './User';
 import TripRepository from './TripRepository';
 import Trip from './Trip';
 import DestinationRepository from './DestinationRepository';
 import Destination from './Destination';
+import dayjs from 'dayjs'
+import {fetchData, postData} from './fetchAPI'
 
 const domUpdates = {
   generateTrips(trips) {
@@ -28,11 +34,34 @@ const domUpdates = {
     })
   },
   generateHeader(user) {
-    userWelcome.innerText += ` ${user.name}`;
+    userWelcome.innerText = `Welcome ${user.name}`;
     userSpend.innerText = `You've spent $${user.getTotalSpentThisYear()} on trips this year`
+  },
+  generateDestinations(destinations) {
+    tripDestination.innerHTML = ''
+    destinations.data.forEach(place => {
+      tripDestination.innerHTML += `
+        <option value="${place.destination}">${place.destination}</option>
+      `
+    });
   },
   toggleHidden(element) {
     element.classList.toggle('hidden')
+  },
+  showTripRequest(trip, destinations) {
+    let newTrip = new Trip(trip)
+    newTrip.addDestinationDetails(destinations)
+    newTripMessage.innerText = 'Your trip has been requested'
+    newTripCard.innerHTML =
+    `<div class="card" tabIndex="0">
+      <h4>${newTrip.destinationDetails.destination}</h4>
+      <img src="${newTrip.destinationDetails.image}" alt="${newTrip.destinationDetails.alt}"/>
+      <p>Travelers: ${newTrip.travelers} travelers</p>
+      <p>Leaving on: ${newTrip.date}</p>
+      <p>Length: ${newTrip.duration}</p>
+      <p>Estimated cost: $${newTrip.calculateTripCost()}</p>
+      <p>Status: ${newTrip.status}</p>
+    </div>`
   }
 }
 

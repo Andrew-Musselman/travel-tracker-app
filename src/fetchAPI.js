d8cb77c6ff2dbec51512f6ee0333de5b9577a723
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-07 17:33:31 -0700

    Make fetch calls more dynamic

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,11 +1,11 @@
 const fetchData = (path) => {
-  return fetch(path)
+  return fetch(`http://localhost:3001/api/v1/${path}`)
   .then(response => response.json())
   .catch(err => console.log(err))
 }
 
 const postData = (path, content) => {
-  return fetch(path, {
+  return fetch(`http://localhost:3001/api/v1/${path}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(content)
@@ -13,9 +13,9 @@ const postData = (path, content) => {
     if (!response.ok) {
       throw new Error('Error requesting trip, please try again')
     } else {
-      return response.json()
+      return response
     }
   }).catch(err => console.log(err))
 }
 
-export {fetchData}
+export {fetchData, postData}
